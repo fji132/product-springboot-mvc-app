8a3d5434801cb40e6672441fd936fcb27c20a72c
Ayoub Youhad <ayoubyouhad79@gmail.com>
2025-06-21 23:27:30 +0100

    initialize Spring Boot project with required dependencies
    - Added Spring Web, Spring Data JPA, Spring Security
    - Integrated Thymeleaf and Spring Validation
    - Configured H2 and MySQL database drivers
    - Included Lombok for cleaner code
    - Added Spring Security extras and test dependencies

---
+++ src/main/java/ma/youhad/productspringmcv/ProductSpringMcvApplication.java
@@ -0,0 +1,13 @@
+package ma.youhad.productspringmcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSpringMcvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductSpringMcvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/youhad/productspringmcv/ProductSpringMcvApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.youhad.productspringmcv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringMcvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
